1bfea8df1366ea5353bd3ba78189326a0085212c
Samuel <samuelmello287@gmail.com>
2020-10-06 17:01:21 -0300

    Commit inicial

---
+++ src/main/java/br/gov/rj/faeterj/estoque/model/Produto.java
@@ -0,0 +1,41 @@
+package br.gov.rj.faeterj.estoque.model;
+
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Produto {
+
+	@NotBlank
+	private String sku;
+	
+	@NotBlank
+	private String nome;
+	
+	@Size(min = 1, max = 50)
+	private String descricao;
+	
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
